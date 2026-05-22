40a323500da1b38ab1493b92f13e8f99e44dd8d3
Psysin <p.proggen@pm.me>
2026-05-22 21:21:36 +0200

    Weitere Funktion gebaut und Schreibfehler Korrigiert. Dopplungen im Code entfernt

--- terminal-version/main.py
+++ terminal-version/main.py
@@ -2,7 +2,8 @@
 import random
 
 
-# Fuctions
+#### Functions ###
+# Prüft den Tipp gegen die geheime Zahl.
 def check_guess(user_number, secret_number):
     if user_number == secret_number:
         return "correct"
@@ -12,40 +13,41 @@ def check_guess(user_number, secret_number):
         return "too_low"
 
 
+# Prüft, ob die Nutzereingabe eine gültige Zahl ist.
+def is_valid_number(user_number):
+    if not user_number.isdigit() or int(user_number) > 100:
+        return False
+    return True
+
+
 secret_number = random.randint(0, 100)
 attempts = 0
 
 print("Welcome to Secret Number!")
 
+### Gameloop ###
 while True:
     user_number = input("Please enter a number between 0 and 100 or quit for exit: ")
 
     if user_number == "quit":
         break
-    elif not user_number.isdigit():
-        print("Please enter a number.")
+    elif not is_valid_number(user_number):
         continue
 
-    elif int(user_number) > 100:
-        print("Please enter a number between 0 and 100!")
-        continue
-
+    # Convert input string to integer.
     user_number = int(user_number)
+    # Erhöt um 1 pro Durchlauf
     attempts += 1
 
+    # Holt das Ergebnis aus der Funktion check_guess.
     result = check_guess(user_number, secret_number)
-    print(result)
 
-    if user_number == secret_number:
+    if result == "correct":
         print(f"Correct! You needed {attempts} attempts.")
         break
-    elif user_number < secret_number:
+    elif result == "too_low":
         print("Your number is too low.")
-    elif user_number > secret_number:
+    elif result == "too_high":
         print("Your number is too high")
 
 print("Program closed")
-
-print(
-    f"Debug: user_number={user_number}, secret_number={secret_number}, attempts={attempts}"
-)
